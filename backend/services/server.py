98073d9da5436d4622cd8b657bf1b70259adcd6e
Mano Vikram <manovik18@gmail.com>
2025-11-14 01:44:34 +0530

    Added function definition for the gRPC method - ProcessPodcast - to transcribe the audio and summarize it and optionally create a blog post if requested

--- backend/services/server.py
+++ backend/services/server.py
@@ -3,6 +3,9 @@ import os
 
 import grpc
 from dotenv import load_dotenv
+from backend.services.pipeline.summarize import generate_blog_post, summarize_text
+from backend.services.pipeline.tts import generate_tts
+from backend.services.pipeline.whisper import transcribe_audio
 from proto import service_pb2, service_pb2_grpc
 
 
@@ -11,7 +14,33 @@ class PodcastSummarizerService(service_pb2_grpc.PodcastSummarizerServiceServicer
         super().__init__()
 
     def ProcessPodcast(self, request, context):
-        pass
+        # Step 1 - Transcribe Audio
+        transcript = transcribe_audio(audio_bytes=request.audio_bytes, language_hint=request.language_hint)
+
+        # Step 2 - Summarize the transcript
+        bullet_points, summary_paragraph = summarize_text(transcript=transcript)
+
+        # Step 3 - Create a blog post from the transcript [Optional]
+        blog_post = generate_blog_post(transcript=transcript) if request.generate_blog else ""
+
+        # Step 4 - Convert summary to speech (TTS) [Optional]
+        summary_audio_bytes = generate_tts(summary_text=summary_paragraph)
+
+        # Step 5 - Prepare and return the response
+        response = service_pb2.SummarizePodcastResponse(
+            transcript=transcript,
+            bullet_points=bullet_points,
+            summary_paragraph=summary_paragraph,
+            blog_post=blog_post,
+            summary_audio_bytes=summary_audio_bytes,
+            metadata={
+                "transcript_model": "faster_whisper_medium",
+                "summarization_model": "ollama_llama2",
+                "tts_model": "openai_gpt-4o-mini-tts"
+            }
+        )
+
+        return response
 
 def serve():
      load_dotenv()
