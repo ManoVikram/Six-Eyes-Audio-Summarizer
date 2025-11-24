00365a834b75dc7a38fefaf0a7b828d9f72659fa
Mano Vikram <manovik18@gmail.com>
2025-11-25 00:13:39 +0530

    Added code to show the file name and file size when a file is selected

--- components/summarizer.jsx
+++ components/summarizer.jsx
@@ -1,18 +1,34 @@
 "use client"
 
-import { Player } from '@lottiefiles/react-lottie-player'
+import dynamic from 'next/dynamic'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
+
+const Player = dynamic(
+    () => import("@lottiefiles/react-lottie-player").then(mod => mod.Player),
+    { ssr: false }
+)
 
 const Summarizer = () => {
+    const [file, setFile] = useState(null)
+
     return (
         <section className='flex flex-col flex-1 p-6 pt-22 bg-gray-200/50 rounded-4xl gap-6'>
             <div className="flex flex-col flex-6 justify-between items-center w-full p-6 bg-white rounded-4xl gap-8">
-                {/* <Player src="/audio-lottie.json" className='h-32 w-32' autoplay loop /> */}
                 <div className="flex flex-col flex-1 justify-center items-center w-2/4 border-4 border-dotted rounded-2xl gap-4 cursor-pointer">
-                    <Image src="/upload-audio-illustration.svg" alt='upload-audio' height={64} width={64} />
+                    {file ? (
+                        <>
+                            <Player src="/audio-lottie.json" className='h-32 w-32' autoplay loop />
 
-                    <p className='text-sm text-gray-500'>Drop your podcast. I&apos;ll summarize it.</p>
+                            <p className='-mt-8'>[FILE_NAME] - <span>[FILE_SIZE]</span></p>
+                        </>
+                    ) : (
+                        <>
+                            <Image src="/upload-audio-illustration.svg" alt='upload-audio' height={64} width={64} />
+
+                            <p className='text-sm text-gray-500'>Drop your podcast. I&apos;ll summarize it.</p>
+                        </>
+                    )}
                 </div>
 
                 <button className='flex justify-center items-center w-2/4 p-3 bg-black text-white hover:drop-shadow-2xl hover:scale-105 transition-all duration-200 rounded-xl cursor-pointer'>
