86ea4d0b42a5a797e09df7c935bc7dd0c1b39e59
Mano Vikram <manovik18@gmail.com>
2025-11-26 01:43:20 +0530

    Added the helper function to convert the file to base64

--- backend/services/lib/api/helpers.js
+++ backend/services/lib/api/helpers.js
@@ -2,23 +2,31 @@
 
 const API_BASE_URL = process.env.API_BASE_URL;
 
-const convertBytesToBase64 = (file) => { }
+const convertBytesToBase64 = async (file) => {
+    const arrayBuffer = await file.arrayBuffer()
+    const buffer = Buffer.from(arrayBuffer)
+
+    return buffer.toString("base64")
+}
 
 export const processPodcast = async (audioFile, audioFormat, generateBlogPost, generateAudioSummary) => {
-    // Step 1 - Call the API endpoint to generate a transcript and summarize the audio
+    // Step 1 - Convert the file to base64
+    const audio_b64 = await convertBytesToBase64(audioFile)
+
+    // Step 2 - Call the API endpoint to generate a transcript and summarize the audio
     const response = await fetch(`${API_BASE_URL}/api/processaudio`, {
         method: "POST",
         body: JSON.stringify({
-            audio_b64: convertBytesToBase64(audioFile),
+            audio_b64: audio_b64,
             audio_format: audioFormat,
             generate_blog: generateBlogPost,
             generate_tts: generateAudioSummary,
         })
     })
 
-    // Step 2 - Convert the response to JSON
+    // Step 3 - Convert the response to JSON
     const responseData = response.json()
 
-    // Step 3 - Return the response
+    // Step 4 - Return the response
     return responseData
 }
