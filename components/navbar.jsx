8156fff1d0e309e761f573be7eec81a1eb9d5b49
Mano Vikram <manovik18@gmail.com>
2025-11-30 15:53:38 +0530

    Updated the navbar UI to adapt to the screen the user is on

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,11 +1,15 @@
 "use client"
 
 import Image from 'next/image'
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import React from 'react'
 
 const Navbar = () => {
     const router = useRouter()
+    const pathname = usePathname()
+
+    const isHome = pathname === "/"
+    const isSummarizer = pathname === "/summarizer"
 
     return (
         <nav className='fixed top-10 left-10 right-10 z-50 p-2'>
@@ -19,12 +23,12 @@ const Navbar = () => {
                 </div>
 
                 <div className="flex justify-between items-center py-1.5 px-1.5 bg-white rounded-full">
-                    <button className="flex justify-center items-center rounded-full px-4 py-1 bg-black cursor-pointer" onClick={() => router.push("/")}>
-                        <p className='text-white'>Home</p>s
+                    <button className={`flex justify-center items-center rounded-full px-4 py-1 ${isHome && "bg-black"} cursor-pointer`} onClick={() => router.push("/")}>
+                        <p className={isHome ? "text-white" : "text-black"}>Home</p>
                     </button>
 
-                    <button className="flex justify-center items-center rounded-full px-4 py-1 bg-transparent cursor-pointer" onClick={() => router.push("/summarizer")}>
-                        <p className='text-black'>Summarizer</p>
+                    <button className={`flex justify-center items-center rounded-full px-4 py-1 ${isSummarizer && "bg-black"} cursor-pointer`} onClick={() => router.push("/summarizer")}>
+                        <p className={isSummarizer ? "text-white" : "text-black"}>Summarizer</p>
                     </button>
                 </div>
             </div>
